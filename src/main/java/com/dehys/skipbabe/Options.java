fad662b3645abd521b526ca57c247d69b5a496a6
BuildTools <unconfigured@null.spigotmc.org>
2022-02-01 12:13:08 +0100

    added Options to get static configuration properties fast for now. Will be replaced with json files or something similar later on

---
+++ src/main/java/com/dehys/skipbabe/Options.java
@@ -0,0 +1,7 @@
+package com.dehys.skipbabe;
+
+public abstract class Options {
+
+    static String PREFIX = "!";
+    static String PERMISSION_ERROR = "You do not have permission to perform this command!";
+}
